0d71378d12616f4f5868c413ab77ff746c79c5f2
Fernando Camero <fernandocameroborrego@gmail.com>
2026-03-23 20:51:52 +0100

    Terminar enlazar la Base de datos al HomeScreen y arreglos en la interfaz.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gestion_inventario/widgets/product_card.dart';
 import '../services/firebase_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,134 +10,139 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreen();
 }
 
-class _HomeScreen extends State<HomeScreen> {
-  // 🔹 Instanciamos el servicio de Firebase
+class _HomeScreen extends State<HomeScreen>{
   final FirebaseService _firebaseService = FirebaseService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            // Lógica del Botón Menu
-          }, 
-          icon: const Icon(Icons.menu, color: Colors.black,)
-        ),
-        actions: [
-          IconButton(
-            onPressed: () { }, 
-            icon: const Icon(Icons.account_circle),
-          )
-        ],
-        title: const Text(
-          'Menu', 
-          style: TextStyle(
-            fontSize: 20, 
-            fontWeight: FontWeight.w400, 
-            color: Colors.black
+  return Scaffold(
+    body: NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          floating: true,
+          snap: true,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu, color: Colors.black),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.account_circle))
+          ],
+          title: const Text('Menu',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400, color: Colors.black)),
+          centerTitle: true,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(90),
+              
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 4, 20, 0),
+              child: Column(
+                children: [
+                  // Barra buscadora
+                  Container(
+                    height: 46,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFF3F4F6),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Buscar producto...',
+                        hintStyle: TextStyle(color: Color.fromARGB(181, 0, 0, 0)),
+                        prefixIcon: Icon(Icons.search),
+                        border: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(vertical: 13),
+                      ),
+                    ),
+                  ),
+                    
+
+                  // Botones
+                  Row(children: [
+                      const SizedBox(width: 15),
+
+                      OutlinedButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.filter_list, color: Color.fromARGB(255, 98, 98, 98)),
+                        label: const Text('Filtro'),
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide.none,
+                          elevation: 3,
+                          backgroundColor: const Color.fromRGBO(247, 242, 250, 1),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                          minimumSize: const Size(0, 1),
+                        ),
+                      ),
+
+                      const SizedBox(width: 170),
+
+                      OutlinedButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.add, color: Color.fromRGBO(8, 102, 255, 1)),
+                        label: const Text('Nuevo'),
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide.none,
+                          elevation: 3,
+                          backgroundColor: const Color.fromRGBO(247, 242, 250, 1),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                          minimumSize: const Size(0, 1),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Barra buscadora
-            Container(
-              height: 46,
-              decoration: BoxDecoration(
-                color: const Color(0xFFF3F4F6),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: const TextField(
-                decoration: InputDecoration(
-                  hintText: 'Buscar producto...',
-                  hintStyle: TextStyle(color: Color.fromARGB(181, 0, 0, 0)),
-                  prefixIcon: Icon(Icons.search, color: Color.fromARGB(255, 0, 0, 0)),
-                  border: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(vertical: 13),
-                ),
-              ),
-            ),
+      ],
 
-            const SizedBox(height: 10,),
+    
 
-            // Botón Filtro + Botón Nuevo
-            Row(
-              children: [
-                OutlinedButton.icon(
-                  onPressed: () {
-                    // Lógica botón filtro
-                  },
-                  icon: const Icon(Icons.filter_list, color: Color(0xFF626262),),
-                  label: const Text('Filtro', style: TextStyle(color: Color.fromARGB(225, 0, 0, 0)),),
-                ),
-                const SizedBox(width: 160,),
-                OutlinedButton.icon(
-                  onPressed: () {
-                    // Lógica botón Nuevo
-                  }, 
-                  label: const Text('Nuevo', style: TextStyle(color: Color.fromARGB(225, 0, 0, 0)),),
-                  icon: const Icon(Icons.add, color: Color.fromRGBO(8, 102, 255, 1)),
-                )
-              ],
-            ),
+            //🔹 Lista de productos desde Firebase
+  body: 
+    StreamBuilder<QuerySnapshot>(
+      stream: _firebaseService.getProductos(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+         return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return const Center(child: Text('Error al cargar productos'));
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No hay productos'));
+        }
+                    
+        final productosFirebase = snapshot.data!.docs;
 
-            const SizedBox(height: 20),
+        return 
+          GridView.builder(
+            padding: EdgeInsets.all(20),
+            clipBehavior: Clip.none,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 12,
+            crossAxisSpacing: 12,
+            ), 
 
-            // 🔹 Lista de productos desde Firebase
-            StreamBuilder<QuerySnapshot>(
-              stream: _firebaseService.getProductos(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.hasError) {
-                  return const Center(child: Text('Error al cargar productos'));
-                }
-
-                final productos = snapshot.data!.docs;
-
-                if (productos.isEmpty) {
-                  return const Center(child: Text('No hay productos'));
-                }
-
-                return Column(
-                  children: productos.map((prod) {
-                    return Card(
-                      margin: const EdgeInsets.symmetric(vertical: 8),
-                      child: ListTile(
-                        leading: prod['imageUrl'] != null && prod['imageUrl'] != ''
-                            ? Image.network(
-                                prod['imageUrl'],
-                                width: 60,
-                                height: 60,
-                                fit: BoxFit.cover,
-                              )
-                            : Container(
-                                width: 60,
-                                height: 60,
-                                color: Colors.grey[300],
-                                child: const Icon(Icons.image),
-                              ),
-                        title: Text(prod['nombre'] ?? ''),
-                        subtitle: Text(prod['descripcion'] ?? ''),
-                        trailing: Text("${prod['precio'] ?? 0}€"),
-                      ),
-                    );
-                  }).toList(),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+            itemCount: productosFirebase.length,  
+            itemBuilder: (context, index){
+            final producto = productosFirebase[index].data() as Map<String, dynamic>;
+            return ProductCard(
+            nombre: producto["nombre"] as String,
+            precio: producto["precio"] as double,
+            );
+          }
+        );
+      },
+    ),
+  )
+);
+}
 }
