fbda4384fc781968ffeb70e9769d73ad62021f23
Diab Miró Miró <diab.walid@hotmail.com>
2026-03-24 09:40:46 +0100

    Añadido implmentacion de cerrar sesion

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:gestion_inventario/screens/login_screen.dart';//Quitar import de la Home debería de estar en LoginScreen
+import 'package:gestion_inventario/screens/login_screen.dart'; //Quitar import de la Home debería de estar en LoginScreen
 import 'package:firebase_core/firebase_core.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-   
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -22,6 +20,6 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
       ),
       home: const LoginScreen(),
-      );
+    );
   }
 }
